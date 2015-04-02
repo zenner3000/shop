7328ce2c88ec54ca0a47de1e112ca1a98a4fb5bc
zeroway <zenner3000@hotmail.com>
2015-04-02 20:49:31 +0800

    到公司要配置和家里一样的
    省得麻烦

--- admin/config.php
+++ admin/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:11111/opencart/admin/');
-define('HTTP_CATALOG', 'http://localhost:11111/opencart/');
+define('HTTP_SERVER', 'http://localhost/shop/admin/');
+define('HTTP_CATALOG', 'http://localhost/shop/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:11111/opencart/admin/');
-define('HTTPS_CATALOG', 'http://localhost:11111/opencart/');
+define('HTTPS_SERVER', 'http://localhost/shop/admin/');
+define('HTTPS_CATALOG', 'http://localhost/shop/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/wamp/www/opencart/admin/');
-define('DIR_SYSTEM', 'D:/wamp/www/opencart/system/');
-define('DIR_LANGUAGE', 'D:/wamp/www/opencart/admin/language/');
-define('DIR_TEMPLATE', 'D:/wamp/www/opencart/admin/view/template/');
-define('DIR_CONFIG', 'D:/wamp/www/opencart/system/config/');
-define('DIR_IMAGE', 'D:/wamp/www/opencart/image/');
-define('DIR_CACHE', 'D:/wamp/www/opencart/system/cache/');
-define('DIR_DOWNLOAD', 'D:/wamp/www/opencart/system/download/');
-define('DIR_UPLOAD', 'D:/wamp/www/opencart/system/upload/');
-define('DIR_LOGS', 'D:/wamp/www/opencart/system/logs/');
-define('DIR_MODIFICATION', 'D:/wamp/www/opencart/system/modification/');
-define('DIR_CATALOG', 'D:/wamp/www/opencart/catalog/');
+define('DIR_APPLICATION', 'D:/wamp/www/shop/admin/');
+define('DIR_SYSTEM', 'D:/wamp/www/shop/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/shop/admin/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/shop/admin/view/template/');
+define('DIR_CONFIG', 'D:/wamp/www/shop/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/shop/image/');
+define('DIR_CACHE', 'D:/wamp/www/shop/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/shop/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/shop/system/upload/');
+define('DIR_LOGS', 'D:/wamp/www/shop/system/logs/');
+define('DIR_MODIFICATION', 'D:/wamp/www/shop/system/modification/');
+define('DIR_CATALOG', 'D:/wamp/www/shop/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '112233');
 define('DB_DATABASE', 'opencart');
 define('DB_PREFIX', 'oc_');
--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:11111/opencart/');
+define('HTTP_SERVER', 'http://localhost/shop/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:11111/opencart/');
+define('HTTPS_SERVER', 'http://localhost/shop/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/wamp/www/opencart/catalog/');
-define('DIR_SYSTEM', 'D:/wamp/www/opencart/system/');
-define('DIR_LANGUAGE', 'D:/wamp/www/opencart/catalog/language/');
-define('DIR_TEMPLATE', 'D:/wamp/www/opencart/catalog/view/theme/');
-define('DIR_CONFIG', 'D:/wamp/www/opencart/system/config/');
-define('DIR_IMAGE', 'D:/wamp/www/opencart/image/');
-define('DIR_CACHE', 'D:/wamp/www/opencart/system/cache/');
-define('DIR_DOWNLOAD', 'D:/wamp/www/opencart/system/download/');
-define('DIR_UPLOAD', 'D:/wamp/www/opencart/system/upload/');
-define('DIR_MODIFICATION', 'D:/wamp/www/opencart/system/modification/');
-define('DIR_LOGS', 'D:/wamp/www/opencart/system/logs/');
+define('DIR_APPLICATION', 'D:/wamp/www/shop/catalog/');
+define('DIR_SYSTEM', 'D:/wamp/www/shop/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/shop/catalog/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/shop/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/wamp/www/shop/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/shop/image/');
+define('DIR_CACHE', 'D:/wamp/www/shop/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/shop/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/shop/system/upload/');
+define('DIR_MODIFICATION', 'D:/wamp/www/shop/system/modification/');
+define('DIR_LOGS', 'D:/wamp/www/shop/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '112233');
 define('DB_DATABASE', 'opencart');
 define('DB_PREFIX', 'oc_');
