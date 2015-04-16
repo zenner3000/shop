c9e22190c49f0172a061fe28914493e63f680fd9
zenner3000 <zenner3000@gmail.com>
2015-04-16 17:45:48 +0800

    首页分析
    中间货物列表分析

--- catalog/model/extension/module.php
+++ catalog/model/extension/module.php
@@ -1,5 +1,8 @@
 <?php
+
 class ModelExtensionModule extends Model {
+
+	//获取到指定模块的信息，这些信息存在一个 setting的字段里面，看上去像是json信息
 	public function getModule($module_id) {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "module WHERE module_id = '" . (int)$module_id . "'");
 		
